115e88a3306d5a6e2176b908b1c856c1613effa6
Sahana N <nsahana@SahanassonalMBP.bbrouter>
2023-09-17 16:43:58 +0530

    Create RESTful springboot backstage template

---
+++ skeleton/src/main/java/com/example/${{values.java_package_name}}/service/GreetingService.java
@@ -0,0 +1,11 @@
+package com.example.backstage.${{values.java_package_name}}.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String greet() {
+        return "Hello from BITS!";
+    }
+}
